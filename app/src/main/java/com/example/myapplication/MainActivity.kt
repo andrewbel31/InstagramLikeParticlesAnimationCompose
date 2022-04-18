abe62a250b75604ced536d6a14459ec60e095fbf
Andrei <andreyrabo@mail.ru>
2022-04-18 19:20:23 +0100

    update libs

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,10 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -39,6 +40,7 @@ class MainActivity : ComponentActivity() {
 
                     Button(
                         onClick = { showParticles = !showParticles }) {
+                        Text(text = "start")
                     }
                 }
             }
